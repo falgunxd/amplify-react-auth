4e61504549c0cb99a103f9677047e7c207ae9a52
Falgun Soni <falgunsoni.2022@gmail.com>
2024-06-12 05:55:54 +0530

    fix amp auth resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,10 +4,9 @@ export const auth = defineAuth({
   loginWith: {
     phone: true,
   },
-  
   multifactor: {
     mode: 'REQUIRED',
     sms: true
   },
-
+  accountRecovery: "PHONE_WITHOUT_MFA_AND_EMAIL"
 });
